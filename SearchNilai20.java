752a68b21573cdeb1438b2c34251e301e49d7d5a
Naufal-farhan <nefener123@gmail.com>
2025-11-12 19:17:43 +0700

    Modifikasi Percobaan-4

--- SearchNilai20.java
+++ SearchNilai20.java
@@ -1,20 +1,32 @@
 import java.util.*;
+
 public class SearchNilai20 {
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-        int[] arrNilai = {80,85,78,96,90,82,86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan berapa jumlah indeks : ");
+        int n = sc.nextInt();
+        int[] arrNilai = new int[n];
+        int j = 0;
+        for (j = 0; j < arrNilai.length; j++) {
+            System.out.print("Masukkan nilai pada indeks ke-" + j + " : ");
+            arrNilai[j] = sc.nextInt();
+        }
 
-        for(int i = 0;i < arrNilai.length;i++){
-            if(key == arrNilai[i]){
-                hasil = i;
+        System.out.print("Masukkan nilai yang anda ingin cari : ");
+        int key = sc.nextInt();
+        int hasil = 0;
+        j = 0;
+
+        for (j = 0; j < arrNilai.length; j++) {
+            if (key == arrNilai[j]) {
+                hasil = j;
+                System.out.println("Nilai " + key + " ditemukan pada indeks ke-" + hasil);
                 break;
+            } else if(j == (n-1)){
+                System.out.println();
+                System.out.println("Nilai tidak ditemukan!");
             }
         }
-    System.out.println();
-    System.out.println("Nilai "+key+" ditemukan pada indeks ke-"+hasil);
-    System.out.println();
     }
-    
+
 }
